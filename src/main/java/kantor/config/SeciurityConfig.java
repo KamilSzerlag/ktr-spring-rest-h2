6aed261472eff705e6522fabdde7eba722889e93
KamilSzerlag <szerlag.kamil@gmail.com>
2018-12-01 00:38:26 +0100

    ohh gash...

--- src/main/java/kantor/config/SeciurityConfig.java
+++
@@ -1,6 +0,0 @@
-package kantor.config;
-
-
-/// TODO: 2018-11-29 Create Security Configuration
-public class SeciurityConfig  {
-}
---
+++ src/main/java/kantor/config/SecurityWebInitializer.java
@@ -0,0 +1,8 @@
+package kantor.config;
+
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/// TODO: 2018-11-29 Create Security Configuration
+public class Seciurity extends AbstractSecurityWebApplicationInitializer {
+}
