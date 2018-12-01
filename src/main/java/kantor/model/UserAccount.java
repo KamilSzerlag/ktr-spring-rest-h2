11b3b782b88d144aad9862c79b499afcaa4ccd31
KamilSzerlag <szerlag.kamil@gmail.com>
2018-12-01 10:17:33 +0100

    work in progress

--- src/main/java/kantor/model/UserAccount.java
+++ src/main/java/kantor/model/UserAccount.java
@@ -5,34 +5,43 @@ import com.sun.istack.internal.NotNull;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "Acount")
-public class CustomerAcount {
+@Table(name = "accounts")
+public class CustomerAccount {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_id")
     int id;
 
     @NotNull
+    @Column(name = "name")
     private String name;
 
     @NotNull
+    @Column(name = "surname")
     private String surname;
 
     @NotNull
+    @Column(name = "login")
     private String login;
 
     @NotNull
+    @Column(name = "password")
     private String password;
 
     @NotNull
+    @Column(name = "email")
     private String email;
+
+    @OneToOne
+    @JoinTable(name = "wallets", joinColumns = @JoinColumn(name = "account_id"), inverseJoinColumns = @JoinColumn(name = "wallet_id"))
     private Wallet wallet;
 
     public String getName() {
         return name;
     }
 
-    public CustomerAcount setName(String name) {
+    public CustomerAccount setName(String name) {
         this.name = name;
         return this;
     }
@@ -41,7 +50,7 @@ public class CustomerAcount {
         return surname;
     }
 
-    public CustomerAcount setSurname(String surname) {
+    public CustomerAccount setSurname(String surname) {
         this.surname = surname;
         return this;
     }
@@ -50,7 +59,7 @@ public class CustomerAcount {
         return login;
     }
 
-    public CustomerAcount setLogin(String login) {
+    public CustomerAccount setLogin(String login) {
         this.login = login;
         return this;
     }
@@ -59,7 +68,7 @@ public class CustomerAcount {
         return email;
     }
 
-    public CustomerAcount setEmail(String email) {
+    public CustomerAccount setEmail(String email) {
         this.email = email;
         return this;
     }
